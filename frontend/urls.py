3b6895c373fc0642dffe957e5250eae7158a9f06
Olamide <leke.olamide@gmail.com>
2021-01-27 10:14:34 +0100

    first commit

---
+++ frontend/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from frontend import views
+
+app_name = 'frontend'
+
+urlpatterns = [
+    path('', views.buy, name='buy'),
+    path('contact-page', views.contact, name='contact'),
+    path('properties-page', views.properties, name='properties'),
+    path('rent-page', views.rent, name='rent'),
+    path('login-page', views.login, name='login'),
+]
