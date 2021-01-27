e81b4b3831d06e326fa6cc12ed0401affefbb69b
toyman640 <falakoadebayor@gmail.com>
2021-01-27 14:00:11 +0100

    mychange

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    f_name = models.CharField(max_length=150, verbose_name='First name')
+    l_name = models.CharField(max_length=150, verbose_name='Last name')
+    email = models.CharField(max_length=150, verbose_name='Email')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_image = models.ImageField(null=True, verbose_name='Image', blank=True, upload_to='uploads/')
+
+    def __str__(self):
+        return self.user.username
+
+# class SalePost(models.Model):
+#     pst_image = models.ImageField(null=True, verbose_name='House Image', blank=True, upload_to='uploads/')
+#     pst_title = models.CharField(max_length=150, verbose_name='')
