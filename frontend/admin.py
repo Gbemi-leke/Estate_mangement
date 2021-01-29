463292285aad8f594d59d50f0f3b331c709909c2
toyman640 <falakoadebayor@gmail.com>
2021-01-29 14:05:24 +0100

    my change

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from frontend.models import *
 
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Category)
+admin.site.register(Sale)
--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    f_name = models.CharField(max_length=100, verbose_name='First name')
+    l_name = models.CharField(max_length=100, verbose_name='Last name')
+    username = models.CharField(max_length=100, verbose_name='User name')
+    email = models.CharField(max_length=100, verbose_name='Email')
+
+    def __str__(self):
+        return self.username
+
+class Category(models.Model):
+    for_sale = models.CharField(blank=True, null=True, max_length=100, verbose_name='Sale or rent')
+    
+
+    def __str__(self):
+        return self.for_sale
+
+    class Meta():
+        verbose_name_plural='Category'
+
+class Sale(models.Model):
+    pst_img = models.ImageField(null=True, verbose_name='Image 1', blank=True, upload_to='uploads/')
+    pst_img = models.ImageField(null=True, verbose_name='Image 2', blank=True, upload_to='uploads/')
+    pst_img = models.ImageField(null=True, verbose_name='Image 3', blank=True, upload_to='uploads/')
+    pst_title = models.CharField(max_length=100, verbose_name='Type of property')
+    price = models.PositiveIntegerField(verbose_name='Price',)
+    cat_id = models.ManyToManyField(Category, verbose_name='Category')
+    bath = models.PositiveIntegerField(verbose_name='Num of Bathrooms',)
+    restroom = models.PositiveIntegerField(verbose_name='Num of Toilets',)
+    more = models.CharField(max_length=100, verbose_name='More information')
+
+
+
+
